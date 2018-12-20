9792f87f0eaed6e0d4684979eb8f265caf9b619d
gaoluo <luo.gao@pccw.com>
2018-12-20 17:30:53 +0800

    修改

--- src/page/Chat.jsx
+++ src/page/Chat.jsx
@@ -10,13 +10,45 @@ function ticking(){
     document.getElementById('root')
   );
 }
+  // var wen  = document.getElementById('wenwen');
+	// function _touch_start(event){
+  //       event.preventDefault();
+  //       $('.wenwen_text').css('background','#c1c1c1');
+  //       $('.wenwen_text span').css('color','#fff');
+  //       $('.saying').show();
+  //   }
+
+  //   function _touch_end(event){
+  //       event.preventDefault();
+  //       $('.wenwen_text').css('background','#fff');
+  //       $('.wenwen_text').css('color','#666');    
+  //       $('.saying').hide();
+  //       var str  = '<div class="question">';
+	//         str += '<div class="heard_img right"><img src="images/dglvyou.jpg"/></div>';
+	//         str += '<div class="question_text clear"><p>不好意思，我听不清！</p><i></i>';
+	//         str += '</div></div>';
+	// 	// $('.contentes').append(str);
+	// 	// for_bottom();
+  //   //     setTimeout(function(){
+  //   //     	var ans  = '<div class="answer"><div class="heard_img left"><img src="images/dglvyou.jpg"/></div>';
+	//   //       	ans += '<div class="answer_text"><p>我不知道你在说什么?</p><i></i>';
+	//   //   		ans += '</div></div>';
+	//   //   	$('.speak_box').append(ans);
+	// 	// 	for_bottom();
+  //   //     },1000);       
+  //   }
+
+  //   wen.addEventListener("touchstart", _touch_start, false);
+  //   wen.addEventListener("touchend", _touch_end, false);
 export default class Chat extends React.Component {
   constructor() {
     super()
     this.state = {
       meg: '',
       respon: [],
-      megArray: []
+      megArray: [],
+      isWrite:true,
+      saying:false
     }
     
 setInterval(ticking, 1000);
@@ -28,45 +60,61 @@ setInterval(ticking, 1000);
   }
   sendMessage() {
     
-    var message = this.state.meg
-    if (message === '') {
-      alert('不能发送空白消息哦')
-    } else {
-      this.setState({
-        megArray: [...this.state.megArray, message]
-      })
-      var that = this
-      var func = fetch('http://www.tuling123.com/openapi/api?key=f0d11b6cae4647b2bd810a6a3df2136f&info=' + message, {
-        method: 'POST',
-        type: 'cors'
-      }).then(function(response) {
-        return response.json();
-        //return "hello";//response.json()
-     }).then(function(detail) {
-        if(detail.code===100000){
-          return (that.setState({respon: [...that.state.respon, detail.text]}, () => {
-            // var el = ReactDOM.findDOMNode(that.refs.msgList);
-            // el.scrollTop=el.scrollHeight;
-            let anchorElement = document.getElementById("scrolld");
-            anchorElement.scrollIntoView();
-          }))
-        }else{
-          return (that.setState({respon: [...that.state.respon, "不知道你说什么,好像服务器发生错误"]}, () => {
-            // var el = ReactDOM.findDOMNode(that.refs.msgList);
-            // el.scrollTop=el.scrollHeight;
-            let anchorElement = document.getElementById("scrolld");
-            anchorElement.scrollIntoView();
-          }))
+      var message = this.state.meg
+      if (message === '') {
+        alert('不能发送空白消息哦')
+      } else {
+        this.setState({
+          megArray: [...this.state.megArray, message]
+        })
+        var that = this
+        var func = fetch('http://www.tuling123.com/openapi/api?key=f0d11b6cae4647b2bd810a6a3df2136f&info=' + message, {
+          method: 'POST',
+          type: 'cors'
+        }).then(function(response) {
+          return response.json();
+          //return "hello";//response.json()
+      }).then(function(detail) {
+          if(detail.code===100000){
+            return (that.setState({respon: [...that.state.respon, detail.text]}, () => {
+              // var el = ReactDOM.findDOMNode(that.refs.msgList);
+              // el.scrollTop=el.scrollHeight;
+              let anchorElement = document.getElementById("scrolld");
+              anchorElement.scrollIntoView();
+            }))
+          }else{
+            return (that.setState({respon: [...that.state.respon, "不知道你说什么,好像服务器发生错误"]}, () => {
+              // var el = ReactDOM.findDOMNode(that.refs.msgList);
+              // el.scrollTop=el.scrollHeight;
+              let anchorElement = document.getElementById("scrolld");
+              anchorElement.scrollIntoView();
+            }))
+          }
+        })
+        this.state.meg = ''
+      }
+    }
+    onInputKeyUp(e){
+        if(e.keyCode === 13){
+          this.sendMessage();
         }
-      })
-      this.state.meg = ''
     }
-  }
-  onInputKeyUp(e){
-    if(e.keyCode === 13){
-      this.sendMessage();
+    changeSpeack(){
+      let isw=this.state.isWrite;
+      if(isw){
+        this.setState({isWrite:false});
+      }else{
+        this.setState({isWrite:true});
+      }
     }
-}
+    _touch_start(event){
+            event.preventDefault();
+          //  document.getElementsByClassName('.wenwen_text').css('background','#c1c1c1');
+          //  document.getElementsByClassName('.wenwen_text span').css('color','#fff');
+           //document.getElementsByClassName('.saying').show();
+           this.setState({saying:true});
+    }
+
   render() {
     var meg = this.state.meg
     var megArray = this.state.megArray
@@ -95,12 +143,19 @@ setInterval(ticking, 1000);
             )
            )}
         </ul>
+        {this.state.saying==true?<div className="saying">
+          <img src={require("../assets/saying.gif")}/>
+        </div>:''}
         <div className="footer">
             <div className="user_face_icon">
-              <img src={require("../assets/jp_btn.png")}/>
+              <img src={this.state.isWrite==true?require("../assets/jp_btn.png"):require("../assets/yy_btn.png")} onClick={()=>this.changeSpeack()}/>
             </div>
+            {this.state.isWrite==true? <div>
             <input id="text" type="text" placeholder="说点什么吧..." value={meg} onChange={this.handleData.bind(this)} onKeyUp={e => this.onInputKeyUp(e)}/>
             <span id="btn"  onClick={this.sendMessage.bind(this)} >发送</span>
+            </div>
+            : <div className="wenwen_text" id="wenwen" onClick={()=>this._touch_start(event)}>  按住 说话  </div>
+            }
         </div>
         <div id="scrolld"> </div>
          {/* <div className="fixedBottom">
