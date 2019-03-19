8fd69c56a206d3289d207e844f317d641258985f
gaoluo <luo.gao@pccw.com>
2019-03-19 16:43:33 +0800

    登录验证

--- src/page/Layout.jsx
+++ src/page/Layout.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import Loadable from 'react-loadable';
 import loading from '../util/loading.jsx'
-//import './Layout.scss';
+import LocalStorge from '../util/LogcalStorge.jsx';
 import WxTabBar from '../components/TabBar';
+const localStorge = new LocalStorge();
 
 export default class MainLoyout extends React.Component {
     constructor(props) {
         super(props)
     }
-    
+    componentDidMount() {
+        let userInfo=localStorge.getStorage('userInfo');
+        if(undefined==userInfo || null==userInfo || ''==userInfo){
+          window.location.href="#/My";
+        }
+    }
+    componentWillReceiveProps(nextProps){
+        let userInfo=localStorge.getStorage('userInfo');
+        if(undefined==userInfo || null==userInfo || ''==userInfo){
+          window.location.href="#/My";
+        }
+    }
     render() {
         return (
             <div >
