c6ee8a36ce7ff5615564a23841222414dd55de33
gaoluo <luo.gao@pccw.com>
2019-04-02 12:06:19 +0800

    保存

--- src/page/Chat.jsx
+++ src/page/Chat.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Widget, addResponseMessage, addLinkSnippet, addUserMessage, renderCustomComponent } from 'react-chat-widget';
+import { Widget, addResponseMessage, toggleWidget,isWidgetOpened,toggleChat,addLinkSnippet, addUserMessage, renderCustomComponent } from 'react-chat-widget';
 import 'react-chat-widget/lib/styles.css';
 import ai from './../assets/icon/ai.png';
 import HttpService from '../util/HttpService.jsx';
@@ -10,25 +10,10 @@ import { List, ListView, PullToRefresh, WhiteSpace, WingBlank, Toast,Brief, Chec
 import { Link, Redirect } from 'react-router-dom';
 import 'antd-mobile/dist/antd-mobile.css';
 import './Chat.css';
+import LocalStorge from '../util/LogcalStorge.jsx';
+const localStorge = new LocalStorge();
 
 const Item = List.Item;
-// function ticking(){
-//   const element = (new Date().toLocaleTimeString());
-//   ReactDOM.render(
-//     element,
-//     document.getElementById('root')
-//   );
-// }
-
-//查看历史信息使用下列加载 开始
-function genData(dataArr) {
-  // const dataArr = [];
-  for (let i = 0; i < 10; i++) {
-    dataArr.push(i);
-  }
-  return dataArr;
-}
-//查看历史信息使用下列加载 结束
 
 export default class Chat extends React.Component {
   constructor(props) {
@@ -41,27 +26,36 @@ export default class Chat extends React.Component {
       saying: false,
       refreshing: false,
       down: true,
-      height: document.documentElement.clientHeight,
       data: [],
-      out:[]
+      out:[],
+      userId:'',
+      to_userId:'0',
+      pageNumd: 1, perPaged: 10
     }
-    // setInterval(ticking, 1000);
   }
-  handleNewUserMessage = (newMessage) => {
-    console.log(`New message incoming! ${newMessage}`);
-    // Now send the message throught the backend API
-    addResponseMessage("response");
-  }
-
-  handleData(e) {
-    this.setState({
-      meg: e.target.value
+  
+ 
+  componentDidMount() {
+    let userInfo = localStorge.getStorage('userInfo');
+    let user_id=null;
+    if (undefined != userInfo && null != userInfo && '' != userInfo) {
+      user_id=userInfo.id;
+      this.setState({ userId:userInfo.id});
+    }else{
+      window.location.href="/My";
+    }
+    let mInfo={'from_userId':user_id,'to_userId':this.state.to_userId,
+              pageNumd:this.state.pageNumd,perPaged:this.state.perPaged}
+    HttpService.post('/reportServer/chat/getChatByuserID', JSON.stringify(mInfo))
+    .then(res => {
+      if (res.resultCode != "1000") {
+       
+      }
     })
   }
   FormD = ({ data, out }) => {
     return <Card style={{backgroundColor:'#f4f7f9'}}>
       <List>
-
         {data.map(val => (
           <Item
             multipleLine
@@ -77,99 +71,90 @@ export default class Chat extends React.Component {
           </Item>
         ))}
       </List>
-
     </Card>
   }
 
   handleModalDataChange(event) {
     this.setState({ test: event.target.value })
   }
- 
-
-
+  //发送消息
   async sendMessage(newMessage){ 
-    //首先进行函数查询
-  await  HttpService.post('/reportServer/nlp/getResult/' + newMessage, null)
-      .then(res => {
-        if (res.resultCode == "1000") {
-          // this.setState({ data: res.data.list, out: res.data.out })
-          return renderCustomComponent(this.FormD, {data: res.data.list, out: res.data.out }); 
-        } else {
-          // Toast.fail(res.message);
-        }
-      })
-      .catch((error) => {
-        // Toast.fail(error);
-      });
+    var ist=true; 
+    //先保存发送信息
+    let userInfo={'from_userId':this.state.userId,
+                  'to_userId':this.state.to_userId,
+                  'post_message':newMessage,
+                  'message_type':'0',
+                  'message_state':'0'
+                }
+    await HttpService.post('/reportServer/chat/createChat', JSON.stringify(userInfo))
+    .then(res => {
+      if (res.resultCode != "1000") {
+        ist=false;
+      }
+    })
+    if(ist){
+      //首先进行函数查询
+      await HttpService.post('/reportServer/nlp/getResult/' + newMessage, null)
+        .then(res => {
+          if (res.resultCode == "1000") {
+            // this.setState({ data: res.data.list, out: res.data.out })
+            //数据保存到数据库
+            let responseInfo={'from_userId':this.state.to_userId,
+                  'to_userId':this.state.userId,
+                  'post_message':res,
+                  'message_type':'0',
+                  'message_state':'0'
+                }
+           HttpService.post('/reportServer/chat/createChat', JSON.stringify(responseInfo))
+            .then(res => {
+              if (res.resultCode != "1000") {
+                console.log(res);
+              }
+            })
+            return renderCustomComponent(this.FormD, {data: res.data.list, out: res.data.out }); 
+          } else {
+
+          }
+        })
+        .catch((error) => {
+          // Toast.fail(error);
+        });
     // return;
       // alert('await over');
     // return addResponseMessage("hello");
     // return renderCustomComponent(this.FormD, {data: newMessage, action: this.handleModalDataChange }); 
-    var message = this.state.meg
-    if (newMessage === '') {
-      alert('不能发送空白消息哦')
-    } else {
-      // this.setState({
-      //   megArray: [...this.state.megArray, message]
-      // })
+   
+    
       var that = this
-      var func = fetch('http://www.tuling123.com/openapi/api?key=f0d11b6cae4647b2bd810a6a3df2136f&info=' + newMessage, {
+      fetch('http://www.tuling123.com/openapi/api?key=f0d11b6cae4647b2bd810a6a3df2136f&info=' + newMessage, {
         method: 'POST',
         type: 'cors'
       }).then(function (response) {
         return response.json();
-        //return "hello";//response.json()
       }).then(function (detail) {
         if (detail.code === 100000) {
+          let responseInfo={'from_userId':that.state.to_userId,
+                  'to_userId':that.state.userId,
+                  'post_message':detail.text,
+                  'message_type':'0',
+                  'message_state':'0'
+                }
+          HttpService.post('/reportServer/chat/createChat', JSON.stringify(responseInfo))
+            .then(res => {
+              if (res.resultCode != "1000") {
+                console.log(res);
+              }
+            })
           return addResponseMessage(detail.text);
-          // (that.setState({respon: [...that.state.respon, detail.text]}, () => {
-          //   // var el = ReactDOM.findDOMNode(that.refs.msgList);
-          //   // el.scrollTop=el.scrollHeight;
-          //   let anchorElement = document.getElementById("scrolld");
-          //   anchorElement.scrollIntoView();
-          // }))
         } else {
-
-          // (that.setState({respon: [...that.state.respon, "不知道你说什么,好像服务器发生错误"]}, () => {
-          //   // var el = ReactDOM.findDOMNode(that.refs.msgList);
-          //   // el.scrollTop=el.scrollHeight;
-          //   let anchorElement = document.getElementById("scrolld");
-          //   anchorElement.scrollIntoView();
-          // }))
         }
       })
+
     }
   }
 
-  componentDidMount() {
-    let btn = document.getElementById("btn");
-    btn.click();
-    // const hei = this.state.height - ReactDOM.findDOMNode(this.lv).offsetTop;
-    // setTimeout(() => {
-    //   this.setState({
-    //     height: hei,
-    //     data: genData(this.state.data),
-    //     height: hei,
-    //     refreshing: false
-    //   });
-    // }, 1500);
-  }
 
-  onRefreshs = () => {
-    this.setState({ refreshing: true });
-    setTimeout(() => {
-      this.setState({
-        refreshing: false,
-        data: genData(this.state.data),
-      });
-    }, 600);
-  }
-
-  onInputKeyUp(e) {
-    if (e.keyCode === 13) {
-      this.sendMessage();
-    }
-  }
   changeSpeack() {
     let isw = this.state.isWrite;
     if (isw) {
@@ -180,21 +165,19 @@ export default class Chat extends React.Component {
   }
   _touch_start(event) {
     event.preventDefault();
-    //  document.getElementsByClassName('.wenwen_text').css('background','#c1c1c1');
-    //  document.getElementsByClassName('.wenwen_text span').css('color','#fff');
-    //document.getElementsByClassName('.saying').show();
     this.setState({ saying: true });
   }
   getCustomLauncher(handleToggle) {
     console.log(handleToggle);
+    console.log(isWidgetOpened);
+    console.log(toggleWidget);
+    isWidgetOpened();
+    //toggleWidget();
+    //Widget.toggleConversation(toggleChat());
     //handleToggle.click();
     // document.getElementById("btn").onclick();
   }
   render() {
-    var meg = this.state.meg
-    var megArray = this.state.megArray
-    var respon = this.state.respon
-
     return (
       <div className="content">
         <Widget
@@ -207,70 +190,13 @@ export default class Chat extends React.Component {
           title="智能机器人"
           subtitle=""
           badge="1"
-          // launcher={handleToggle => this.getCustomLauncher(handleToggle)}
-          launcher={handleToggle => (
-            <input id="btn" onClick={handleToggle} type="button"></input>
-          )}
+          toggleWidget={true}
+         /// launcher={handleToggle =>this.getCustomLauncher(handleToggle)}
+          // launcher={handleToggle => (
+          //   <input id="btn" onClick={handleToggle} type="button"></input>
+          // )}
         />
-        {/* <div className="header">
-            <span style={{float: "left"}}><Link to={`/Main`}><img src={require("../assets/返回.svg")} style={{width:"20px",height:"20px",marginTop:'10px'}}/></Link></span>
-            <span style={{float: "right"}} id="root"></span>
-        </div> */}
-        {/* <ul className="contentes">
-        <PullToRefresh
-            damping={60}
-            ref={el => this.lv = el}
-            style={{
-              height: this.state.height,
-              overflow: 'auto',
-            }}
-            indicator={this.state.down ? {} : { deactivate: '上拉可以刷新' }}
-            direction={this.state.down ? 'down' : 'up'}
-            refreshing={this.state.refreshing}
-            onRefresh={this.onRefreshs}
-        >
-        {this.state.data.map((elem,index) => (
-          // <div key={index} style={{ textAlign: 'center', padding: 20 }}>
-              <div style={{background:'#f5f5f9' }}>
-                   <li ><img src={require("../assets/奥巴马-02.jpg")} className="imgright"/><span style={{float:"right"}}>{elem} </span></li>
-                   <li><img src={require("../assets/川普-01.jpg")} className="imgleft"/><span style={{float:"left",background:'#f1ebeb00'}}>35 ¥ {elem} </span></li>
-               </div>
-          // </div>
-        ))}
         
-      </PullToRefresh>
-          {megArray.map((elem,index) => (
-              <div>
-                  <li><img src={require("../assets/奥巴马-02.jpg")} className="imgright"/><span style={{float:"right"}} >{elem} </span></li>
-                  <li><img src={require("../assets/川普-01.jpg")} className="imgleft"/><span style={{float:"left",background:'#f1ebeb00'}}>{respon[index]} </span></li>
-              </div>
-            // <div className="container" key={index}>
-            //   <div className="message">
-            //       <span>{elem} </span>
-            //       <span><img src={require("../assets/a.png")} width="15" height="15"/></span>
-                
-            //   </div>
-            //   <div className="response">{respon[index]}</div>
-            // </div>
-            )
-           )}
-        </ul>
-        <div id="scrolld" > </div>
-        {this.state.saying==true?<div className="saying">
-          <img src={require("../assets/saying.gif")}/>
-        </div>:''}
-        <div className="footer">
-            <div className="user_face_icon">
-              <img src={this.state.isWrite==true?require("../assets/jp_btn.png"):require("../assets/yy_btn.png")} onClick={()=>this.changeSpeack()}/>
-            </div>
-            {this.state.isWrite==true? <div>
-            <input id="text" type="text" placeholder="说点什么吧..." value={meg} onChange={this.handleData.bind(this)} onKeyUp={e => this.onInputKeyUp(e)}/>
-            <span id="btn"  onClick={this.sendMessage.bind(this)} >发送</span>
-            </div>
-            : <div className="wenwen_text" id="wenwen" onClick={()=>this._touch_start(event)}>  按住 说话  </div>
-            }
-        </div> */}
-        {/* <div id="scrolld"> </div> */}
       </div>
     )
   }
