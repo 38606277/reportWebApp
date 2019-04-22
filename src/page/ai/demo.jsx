af87aee8fe43b2a21af9b8c5720ecf8ea2a0bfa7
gaoluo <luo.gao@pccw.com>
2019-04-22 17:29:05 +0800

    语音

--- src/page/ai/demo.jsx
+++ src/page/ai/demo.jsx
@@ -5,69 +5,96 @@ import Script from 'react-load-script';
 import HttpService from '../../util/HttpService.jsx';
 import LocalStorge from '../../util/LogcalStorge.jsx';
 const localStorge = new LocalStorge();
+import saying from './../../assets/saying.gif';
 var recorder;        
 var msg={};
 var question;
 var answer;
 var audio = document.querySelector('audio');
 var msgId=1;
+
+var timer = "", //定时器
+		startX = 0, //开始x轴坐标
+		startY = 0, //开始y轴坐标
+        isSend = false; //记录是上滑还是下滑，下滑录音
+        
 function initEvent() {
   
     var btnElem=document.getElementById("microphone");//获取ID
     btnElem.addEventListener("touchstart", function(event) {
-        //event.preventDefault();//阻止浏览器默认行为
+        event.preventDefault();//阻止浏览器默认行为
+        document.getElementById("microphone").text="松开  结束";
+		// timer = setTimeout(function() {
+		// 	//$(".issaying").show(); //开始录音时候出现动画
+        //     HZRecorder.get(function (rec) {
+        //         recorder = rec;
+        //         recorder.start();
+        //     });
+		// }, 500);
         HZRecorder.get(function (rec) {
             recorder = rec;
             recorder.start();
         });
     });
-
+    // btnElem.addEventListener("touchmove", function(event) {
+	// 	event.preventDefault();
+	// 	var touch = event.touches[0], //获取第一个触点
+	// 		x = Number(touch.pageX), //页面触点X坐标
+	// 		y = Number(touch.pageY); //页面触点Y坐标
+	// 	//判断滑动方向
+	// 	$(".saying").hide();
+	// });
     btnElem.addEventListener("touchend", function(event) {
-        //event.preventDefault();
+        event.preventDefault();
+        document.getElementById("microphone").text="按住  说话";
+		timer = "";
         HZRecorder.get(function (rec) {
             recorder = rec;
             recorder.stop();
         })
-    
+       // $(".issaying").hide();
         //发送音频片段
         var data=recorder.getBlob();
-        if(data.duration==0){
-            alert("请先录音");
-            return;
-        }
+        // if(data.duration==0){
+        //     alert("请先录音");
+        //     return;
+        // }
     
-    if(data.duration!==0){
-        //导出wav文件
-        //  downloadRecord(data.blob);
-        msg[msgId]=data;
-        recorder.clear();
-        var dur=data.duration/10;
-        var str="<div class='warper'><div id="+msgId+" class='voiceItem' >"+dur+"s</div></div>"
-        $(".messages").append(str);
-        msgId++;
-        let formData = new FormData();
-        formData.append("file", data.blob);
-        HttpService.post("reportServer/MyVoiceApplication/uploadai",formData).then(response=>{
-            if(response.resultCode=="1000"){
-                question=response.data.content;
-                fetch('http://www.tuling123.com/openapi/api?key=f0d11b6cae4647b2bd810a6a3df2136f&info=' + response.data.content, {
-                    method: 'POST',
-                    type: 'cors'
-                }).then(function (response) {
-                    return response.json();
-                }).then(function (detail) {
-                    if (detail.code === 100000) {
-                       answer =detail.text
-                        yuyin();
-                    }
-                });
-            }
-        });
-        $(".voiceItem").click(function(){
-            var id=$(this)[0].id;
-            var data=msg[id];
-            playRecord(data.blob);
-         });
+        if(data.duration!==0){
+            //导出wav文件
+            //  downloadRecord(data.blob);
+            msg[msgId]=data;
+            recorder.clear();
+            var dur=data.duration/10;
+            var str="<div class='warper'><div id="+msgId+" class='voiceItem' >"+dur+"s</div></div>"
+            $(".messages").append(str);
+            msgId++;
+            let formData = new FormData();
+            formData.append("file", data.blob);
+            HttpService.post("reportServer/MyVoiceApplication/uploadai",formData).then(response=>{
+                if(response.resultCode=="1000"){
+                    question=response.data.content;
+                    fetch('https://api.ownthink.com/bot?spoken=' + response.data.content, {
+                        method: 'POST',
+                        type: 'cors'
+                    }).then(function (response) {
+                        return response.json();
+                    }).then(function (detail) {
+                        console.log(detail);
+                        if (detail.message =="success") {
+                        answer =detail.data.info.text
+                            yuyin();
+                        }
+                    });
+                }
+            });
+            $(".voiceItem").click(function(){
+                var id=$(this)[0].id;
+                var data=msg[id];
+                playRecord(data.blob);
+            });
+        }else{
+            console.log("没有声音输入");
         } 
     });
 }
@@ -241,12 +268,30 @@ export default class Demo extends React.Component {
         });
         }
     }
+    handleStart(e){
+        this.setState({
+            saying:true
+        });
+    }
+    handleTouchMove(e) {
+        // this.setState({
+        //     saying:false,
+        // });
+    }
+
+    handleTouchEnd (e) {
+        this.setState({
+            saying:false,
+        });
+    }
     render() {
         return (
             <div>
                 <div class="messages"></div>
                 <Script url="../src/page/ai/jquery-3.2.1.min.js"/>
                 <Script url="../src/page/ai/record.js"/>
+                <Script url="../src/page/ai/wcPop.js"/>
+
                 <InputItem  type="text"  name="answer"  placeholder="输入用户名"  clear
                     onKeyUp={e => this.onInputKeyUp(e)} 
                     onChange={(v) => this.onInputChange('answer', v)}
@@ -255,9 +300,17 @@ export default class Demo extends React.Component {
                 ></InputItem>
 
                 <audio controls autoplay></audio>
+                {/* <div className="issaying"><img src={saying}  className="saying"/></div> */}
+                {this.state.saying==true?<div className="saying">
+                    <img src={require("../../assets/saying.gif")}/>
+                </div>:''}
                 <div style={{textAlign:'center',margin:'10px'}}>
                     <Button type="primary" onClick={()=>this.yuyin()} inline size="small" style={{marginRight: '4px' }}>语音合成</Button>
-                    <Button type="primary" id="microphone" inline size="small">录音</Button>
+                    <Button type="primary" id="microphone"  
+                        onTouchStart={this.handleStart.bind(this)}  //使用bind(this)改变函数作用域，不加上bind则this指向的是全局对象window而报错。
+                        onTouchMove={this.handleTouchMove.bind(this)}
+                        onTouchEnd={this.handleTouchEnd.bind(this)} 
+                        inline size="small">按住  说话</Button> 
                 </div>
             </div>
         );
--- src/page/ai/record.js
+++ src/page/ai/record.js
@@ -167,6 +167,38 @@ var HZRecorder = function (stream, config) {
 //     throw new function () { this.toString = function () { return message; } }
 // }
 //是否支持录音HZRecorder.canRecording = (navigator.getUserMedia != null);
+var promisifiedOldGUM = function(constraints) {
+	// First get ahold of getUserMedia, if present
+	var getUserMedia =
+		navigator.getUserMedia ||
+		navigator.webkitGetUserMedia ||
+		navigator.mozGetUserMedia;
+
+	// Some browsers just don't implement it - return a rejected promise with an error
+	// to keep a consistent interface
+	if (!getUserMedia) {
+		return Promise.reject(
+			new Error('getUserMedia is not implemented in this browser')
+		);
+	}
+
+	// Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+	return new Promise(function(resolve, reject) {
+		getUserMedia.call(navigator, constraints, resolve, reject);
+	});
+};
+
+// Older browsers might not implement mediaDevices at all, so we set an empty object first
+if (navigator.mediaDevices === undefined) {
+	navigator.mediaDevices = {};
+}
+
+// Some browsers partially implement mediaDevices. We can't just assign an object
+// with getUserMedia as it would overwrite existing properties.
+// Here, we will just add the getUserMedia property if it's missing.
+if (navigator.mediaDevices.getUserMedia === undefined) {
+	navigator.mediaDevices.getUserMedia = promisifiedOldGUM;
+}
 //获取录音机
 HZRecorder.get = function (callback, config) {
     if (callback) {  
